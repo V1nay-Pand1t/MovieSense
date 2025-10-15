6f75e26789bafa6192ce53d7a291f93baa78fa80
Vinay Pandit <vinaypanditx8@gmail.com>
2025-10-15 21:42:44 +0530

    Code updates

--- Movie-Recommender/movide_recommender/imdb_picker/urls.py
+++ Movie-Recommender/movide_recommender/imdb_picker/urls.py
@@ -3,12 +3,12 @@ from rest_framework.routers import DefaultRouter
 from imdb_picker.views import MovieViewSet
 from imdb_picker import views
 
+
 router = DefaultRouter()
 router.register(r'movies', MovieViewSet, basename='movie')
 ### lets expose metrics endpoint for prometheus
 
 urlpatterns = [
-    path('metrics/', include('django_prometheus.urls')),
     path('', include(router.urls)),
 ]
 
